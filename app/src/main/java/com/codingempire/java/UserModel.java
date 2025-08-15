3fd66c1ed921441cf60d2ddd7a8de216faa437f0
Rarenayab520 <nayabasmat121@gmail.com>
2025-08-15 21:16:19 +0100

    model clss

--- app/src/main/java/com/codingempire/java/UserModel.java
+++ app/src/main/java/com/codingempire/java/UserModel.java
@@ -1,22 +1,22 @@
 package com.codingempire.java;
 
 public class UserModel {
-        String username,password , email;
+        String username , password,email;
 
-    public UserModel(String userId, String userName, String email) {
+    public UserModel(String password, String userName, String email) {
         this.username = userName;
         this.email = email;
         this.password = password;
     }
 
         public String getUsername() {
-            return username;
+        return username;
         }
         public String getEmail() {
-            return email;
+        return email;
         }
         public String getPassword() {
-            return password;
+        return password;
         }
     }
 
